d24edb6b0648b8e9544d3177cfa852db2fd4e8e6
zozulia-developer <kirillzozulya95@gmail.com>
2021-07-04 16:09:19 +0300

    added logger and new copy implementation

--- util.py
+++ util.py
@@ -2,46 +2,71 @@
 import os
 import shutil
 import argparse
-from threading import Thread, active_count
+import logging
+from threading import Thread, Semaphore
+
+logging.basicConfig(filename='logs.log', filemode='w',
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%H:%M:%S', level=logging.DEBUG)
 
 
-def _is_dir(files: list, src: str, to: str) -> None:
-    for f in files:
-        print(os.path.isdir(src+f))
-        if os.path.isdir(src+f):
-            if not os.path.exists(to+f):
-                os.mkdir(to+f)
-                print(f'Make new directory in {to} successful!')
-            files_in_dir = os.listdir(src+f)
-            return _is_dir(files_in_dir, src + f + '/', to + f + '/')
-        _copy(src+f, to+f)
+def _deep_copy(src: str, to: str, s: Semaphore) -> None:
+    """ Copy files with directories """
+    extension = _check_pattern(src)
+    src_path = src.split('*')[0]
+    files = os.listdir(src_path)
+    for file in files:
+        if extension and file.endswith(extension):
+            t = Thread(target=_copy(src_path + file, to + file), args=(s,))
+            t.start()
+        if not extension and os.path.isfile(src_path+file):
+            t = Thread(target=_copy(src_path+file, to+file), args=(s,))
+            t.start()
+        if os.path.isdir(src_path+file):
+            try:
+                shutil.copytree(src_path+file, to+file)
+                logging.info('Directory %s deep copied to %s successfully!', file, to+file)
+            except FileExistsError:
+                logging.warning('Directory %s already exists!', to+file)
 
 
 def _copy(src: str, to: str) -> None:
+    """ Copy file with logging """
     if os.path.isfile(src):
         shutil.copy2(src, to)
-        print(f'File copied from {src} to {to} successfully!')
+        logging.info('File copied from %s to %s successfully!', src, to)
 
 
-def copy_files(from_path: str, to_path: str, threads_number=1) -> None:
-    files = os.listdir(from_path)
-    print(files)
+def _check_pattern(path: str) -> str:
+    """ Check if path contains symbol '*' """
+    return path.split('*')[1] if '*' in path else None
+
+
+def copy_files(from_path: str, to_path: str, s: Semaphore) -> None:
+    """ Copy files from source path to destination path """
     try:
-        for f in files:
-            _is_dir(files, from_path, to_path)
-            _copy(from_path+f, to_path+f)
-    except FileNotFoundError:
-        pass
+        _deep_copy(from_path, to_path, s)
+    except FileNotFoundError as e:
+        print(e)
 
 
-def move_files(from_path: str, to_path: str, threads_number=1) -> None:
-    files = os.listdir(from_path)
+def move_files(from_path: str, to_path: str, s: Semaphore) -> None:
+    """ Move files from source path to destination path """
+    extension = _check_pattern(from_path)
+    src_path = from_path.split('*')[0]
+    files = os.listdir(src_path)
     try:
-        for f in files:
-            shutil.move(from_path + f, to_path)
-            print(f"File {f} moved from {from_path} to {to_path} successfully!")
-    except FileNotFoundError:
-        pass
+        for file in files:
+            if extension and file.endswith(extension):
+                t = Thread(target=shutil.move(from_path + file, to_path), args=(s,))
+                t.start()
+                logging.info('File %s moved from %s to %s successfully!', file, from_path, to_path)
+            else:
+                t = Thread(target=shutil.move(from_path + file, to_path), args=(s,))
+                t.start()
+                logging.info('File %s moved from %s to %s successfully!', file, from_path, to_path)
+    except FileNotFoundError as e:
+        print(e)
 
 
 def main():
@@ -49,14 +74,17 @@ def main():
     parser.add_argument('-o', '--operation', help='select operation, move or copy')
     parser.add_argument('-f', '--FROM', help='select path where from to do operation')
     parser.add_argument('-t', '--TO', help='select path where to do operation')
-    parser.add_argument('-thr', '--threads', type=int, help='select number of threads')
+    parser.add_argument('-thr', '--threads', type=int, default=1, help='select number of threads')
     args = parser.parse_args()
-    print(args)
+    s = Semaphore(args.threads)
+    logging.info('Amount of Semaphores - %s', args.threads)
 
     if args.operation == 'copy':
-        copy_files(args.FROM, args.TO)
+        copy_files(args.FROM, args.TO, s=s)
     elif args.operation == 'move':
-        move_files(args.FROM, args.TO)
+        move_files(args.FROM, args.TO, s=s)
+    else:
+        logging.error("'%s' is Incorrect operation!", args.operation)
 
 
 if __name__ == '__main__':
